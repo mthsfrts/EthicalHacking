b37ea2d72ab485bbc17a71b47469e0a71f5a6764
matheus <matheus.britto@gmail.com>
2022-08-11 15:33:10 -0300

    Basic Port Scanner TCP protocol on Scapy

--- Scapy/scanner.py
+++ Scapy/scanner.py
@@ -1,33 +1,35 @@
 # Libraries
 from scapy.all import *
-import logging
 import sys
 
-# Logging Errors
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
-# Checking Command Line
-if len(sys.argv) != 4:
-    print("Usage: %s , target, startport, endport" % (sys.argv[0]))
-    sys.exit(0)
+def scapy_scanner():
+    # Checking Command Line
+    if len(sys.argv) != 4:
+        print("Usage: %s , target, startport, endport" % (sys.argv[0]))
+        sys.exit(0)
 
-target = str(sys.argv[1])
-startport = int(sys.argv[2])
-endport = int(sys.argv[3])
-print("Scanning " + target + " for open TCP ports\n")
+    target = str(sys.argv[1])
+    startport = int(sys.argv[2])
+    endport = int(sys.argv[3])
+    print("Scanning " + target + " for open TCP ports\n")
 
-# 3-way handshake
-for ports in range(startport, endport + 1):
-    try:
-        pkt = IP(dst=target) / TCP(dport=ports, flags='S')
-        response = sr1(pkt, timeout=2, verbose=0)
+    # 3-way handshake
+    for ports in range(startport, endport + 1):
+        try:
+            pkt = IP(dst=target) / TCP(dport=ports, flags='S')
+            response = sr1(pkt, timeout=2, verbose=0)
 
-        if response.haslayer(TCP) and response.getlayer(TCP).flags == 0x12:
-            print("Port " + str(ports) + " is open on your destination " + str(target) + "!")
+            if response.haslayer(TCP) and response.getlayer(TCP).flags == 0x12:
+                print("Port " + str(ports) + " is open on your destination " + str(target) + "!")
 
-            sr(IP(dst=target) / TCP(dport=response.sport, flags='R'), timeout=2, verbose=0)
+                sr(IP(dst=target) / TCP(dport=response.sport, flags='R'), timeout=2, verbose=0)
 
-    except (AttributeError, IndexError, ValueError) as error:
-        print(f"The Port - {ports} / Host - {target} is either missing or could not be reached!!")
+        except (AttributeError, IndexError, ValueError) as error:
+            print(f"The Port - {ports} / Host - {target} is either missing or could not be reached!!")
 
-print("Scan is complete!")
+    print("Scan is complete!")
+
+
+if __name__ == "__main__":
+    scapy_scanner()
